0d622360813ce599f3816f9ce5bb062516d04e94
brandonbraun653 <brandonbraun653@gmail.com>
2021-04-07 17:36:25 -0500

    Add layout for planned simulation components

---
+++ projects/steady_state_control.py
@@ -0,0 +1,10 @@
+# **********************************************************************************************************************
+#   FileName:
+#       steady_state_control.py
+#
+#   Description:
+#       Tests the ability of flight software to control the system to straight and level flight
+#
+#   4/7/21 | Brandon Braun | brandonbraun653@gmail.com
+# **********************************************************************************************************************
+
